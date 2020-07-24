ed01df4ad8d6998baea238e21fbc789138841012
SDOO <sdoo@sdoo.cc>
2020-07-24 15:37:05 +0800

    g

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d89260f7fbb333e33a39be0f5e2fdcd8.js"
+  "/precache-manifest.f11aea45352450086f0c662a702f9fee.js"
 );
 
 workbox.clientsClaim();
